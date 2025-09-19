32387019279b6e1eb3610f3d74ccec18c1e12654
jacvan1995 <vantassel.je99@gmail.com>
2025-09-19 03:52:46 -0400

    sync

--- src/pages/full-sreen-camera.jsx
+++ src/pages/full-sreen-camera.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import React, { useRef, useEffect } from "react";
 import "../styles/full-screen.css";
+import CameraIcon from "../assets/image 225 (Traced).svg";
 
 const FullscreenCamera = () => {
   const videoRef = useRef(null);
@@ -42,12 +43,24 @@ const FullscreenCamera = () => {
           muted
           playsInline
         />
-        <div className="overlay-text">
-          <h1>SKINSTRIC [ ANALYSIS ]</h1>
-          <p>TO GET BETTER RESULTS MAKE SURE TO HAVE</p>
-          <p>ADEQUATE LIGHTING</p>
-          <p>FRONTAL POSE</p>
-          <p>NEUTRAL EXPRESSION</p>
+        <div>
+          <nav>
+            <h1 className="logo">
+              SKINSTRIC <span className="light">[ ANALYSIS ]</span>
+            </h1>
+          </nav>
+          <div className="capture-button">
+            <div className="button-text">TAKE PHOTO</div>
+            <button className="capture-inner">
+              <img src={CameraIcon} />
+            </button>
+          </div>
+          <div className="overlay-text">
+            <p>TO GET BETTER RESULTS MAKE SURE TO HAVE</p>
+            <p>ADEQUATE LIGHTING</p>
+            <p>FRONTAL POSE</p>
+            <p>NEUTRAL EXPRESSION</p>
+          </div>
         </div>
       </div>
     </section>
